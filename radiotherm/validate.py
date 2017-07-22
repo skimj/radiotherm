ba300ed4f3771f663357fce6e97f17e99e232b3e
Ryan Nowakowski <tubaman@fattuba.com>
2017-07-22 08:41:03 -0500

    Handle tstat transient errors
    In section 2.7.3 of the developer's guide[1], GETs to /tstat could
    return a -1 tmode and/or temp value.  Throw an exception if we
    encounter those transient errors.
    
    1. https://lowpowerlab.com/downloads/RadioThermostat_CT50_Honeywell_Wifi_API_V1.3.pdf

--- radiotherm/validate.py
+++ radiotherm/validate.py
@@ -11,7 +11,7 @@ def validate_response(response, content=None):
                         This is required if the response was "read" prior to
                         calling this method.
     :type content:      any iterable collection 
-    :returns:           None
+    :returns:           tuple of the validated response and content
     :raises:            AttributeError
     """
     if response.getcode() != 200:
@@ -23,3 +23,25 @@ def validate_response(response, content=None):
     for error_field in ('error_msg', 'error'):
         if error_field in content:
             raise AttributeError('Error message from thermostat: %s' % content[error_field])
+    return response, content
+
+
+def validate_tstat_response(response, content=None):
+    """
+    raises AttributeError if the HTTP response code is not 200, or if the value
+    returned by the server for /tstat indicates an error.
+
+    :param response:    the response returned by a call to urllib.request.urlopen
+    :type response:     file-like object
+    :param content:     the JSON-deserialized value returned by the server.
+                        This is required if the response was "read" prior to
+                        calling this method.
+    :type content:      any iterable collection
+    :returns:           tuple of the validated response and content
+    :raises:            AttributeError
+    """
+    response, content = validate_response(response, content)
+    for field in ('tmode', 'temp'):
+        if content[field] == -1:
+            raise AttributeError('Transient error from thermostat. You should probably just retry.')
+    return response, content
---
+++ tests/validate/test_validate_tstat_response.py
@@ -0,0 +1,28 @@
+import json
+
+from mock import MagicMock
+
+from radiotherm.validate import validate_tstat_response
+from tests.validate.test_validate_response import TestValidateResponse
+
+
+class TestValidateTStatResponse(TestValidateResponse):
+    VALIDATE_RESPONSE = staticmethod(validate_tstat_response)
+    SIMPLE_RETURN_VALUE = {"tmode": 1, "fmode": 2, "temp": 78, "hold": 1}
+    COMPLEX_RETURN_VALUE = SIMPLE_RETURN_VALUE
+    TRANSIENT_ERROR_RETURN_VALUE = {
+        "tmode": -1,
+        "fmode": 2,
+        "temp": -1,
+        "hold": 1
+    }
+
+    def test_json_transient_error(self):
+        response = self.build_mock_response(200,
+                                            self.TRANSIENT_ERROR_RETURN_VALUE)
+        self.assertRaises(AttributeError, self.VALIDATE_RESPONSE, response)
+
+    def test_content_transient_error(self):
+        response = self.build_mock_response(200)
+        self.assertRaises(AttributeError, self.VALIDATE_RESPONSE, response,
+                          self.TRANSIENT_ERROR_RETURN_VALUE)
